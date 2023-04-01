591b163c37076ba076a97257190ff626a41bb0eb
Thandriss <mahonko_yana@mail.ru>
2023-04-01 14:04:58 +0300

    #5 Created Server

---
+++ src/main/kotlin/ya/makhonko/Server.kt
@@ -0,0 +1,19 @@
+package ya.makhonko
+
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import java.util.concurrent.atomic.AtomicBoolean
+
+object Server {
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+    var port = 0
+    var flagFirst = AtomicBoolean(false)
+    var name: String = ""
+    lateinit var toSend: Set<String>
+}
